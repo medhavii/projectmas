510603d4e4a040d1be9624cee68508e115d6bcfb
siddhardha123 <siddhardha123123@gmail.com>
2023-02-04 15:27:54 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mysql from 'mysql'
 import config from './config.js'
 
-const app = express();
+const app =   express();
 const connection = mysql.createConnection(config)
 connection.connect((e)=> {
     if(e) throw console.error(e);
